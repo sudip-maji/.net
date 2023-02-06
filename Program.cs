803a73716fe73d3696fbd474c2abe4600a6ba377
sudip <sudip220701@gmail.com>
2023-02-06 12:13:33 +0530

    Third commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿String fullName = "Sudip Maji";
+// String phoneNumber = "123-456-7890";
+
+
+//fullName = fullName.ToUpper();
+//fullName = fullName.ToLower();
+//Console.WriteLine(fullName);
+
+//phoneNumber = phoneNumber.Replace("-","");
+//Console.WriteLine(phoneNumber);
+
+// String userName = fullName.Insert(0,"Mr.");
+// Console.WriteLine(userName);
+
+//Console.WriteLine(fullName.Length);
+
+// String firstName = fullName.Substring(0, 6);
+//             String lastName = fullName.Substring(6);
+
+//             Console.WriteLine(firstName);
+//             Console.WriteLine(lastName);
+
+
+string m="1,2,3,4,5,6";
+foreach(var c in m.Split(",")){
+    System.Console.WriteLine(c);
+}
+
+string g="1";
+int s =Convert.ToInt32(g);
+System.Console.WriteLine(s);
